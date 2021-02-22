857babd0592eef61e5be5cb50e88562af1c74a90
Jouse then <jthen93@gmail.com>
2021-02-22 21:10:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
   
   def initialize(name, artist, genre)
     @name = name
-    @@artist = artist
-    @@genre = genre
+    @artist = artist
+    @genre = genre
     @@count += 1
     
     if @@artists.include?(artist) == false 
